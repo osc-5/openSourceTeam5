e6adc7dca0a94dd0d4b3f40541105bdabbc9e182
nohgijin <ynda11111@naver.com>
2020-09-02 21:57:53 +0900

    [#8] feat - 월별 표시 월별 데이터가 바뀌도록 표시

--- calendar.js
+++ calendar.js
@@ -1,20 +1,31 @@
 let template=''
-let month=1;
+let month=11;
 const $body=document.querySelector('body')
 conferences.reverse().forEach(conference => {
-    console.log(conference.date[0])
-    template+=`<table class="today">
-        <tbody>
-            <tr class="first">
-                <td class="date">${conference.date}</td>
-                <td class="content" onClick="location.href='${conference.link}'">${conference.content}</td>
-            </tr>
-            <tr class="second">
-                <td class="time">${conference.time}</td>
-                <td class="location">${conference.location}</td>
-            </tr>
-        </tbody>
-    </table>`
+    if(month!=conference.date[0]){
+        month--;
+        template+=`
+            <div class="month">
+                2020년 ${month}월 IT 세미나/컨퍼런스 일정
+            </div>
+        `
+    }
+    template+=`
+    <div class="today">
+        <table>
+            <tbody>
+                <tr class="first">
+                    <td class="date">${conference.date}</td>
+                    <td class="content" onClick="location.href='${conference.link}'">${conference.content}</td>
+                </tr>
+                <tr class="second">
+                    <td class="time">${conference.time}</td>
+                    <td class="location">${conference.location}</td>
+                </tr>
+            </tbody>
+        </table>
+    </div>
+    `
 });
 
 const $target = templateToElement(template)
